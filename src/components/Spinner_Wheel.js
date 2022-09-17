cf06297e909d7637e56317f0f401f093065d1843
Ryan McGowan <vitomaserati@gmail.com>
2022-09-16 18:30:51 -0700

    add suspense

---
+++ src/components/Spinner_Wheel.js
@@ -0,0 +1,11 @@
+import loading_spinner from './Spinner_Wheel.js'
+
+const Spinner = () => {
+    return (
+        <div>
+            <img src={loading_spinner} alt="loading..." />
+        </div>
+    )
+}
+
+export default Spinner
